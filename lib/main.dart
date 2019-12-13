f421002ad56883cc0cbd9731693d589f84a56d80
Jaigouk Kim <jaigouk.kim@blacklane.com>
2019-12-13 11:52:00 +0100

    WIP for displaying json

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,108 @@
 import 'package:flutter/material.dart';
 // import 'package:english_words/english_words.dart';
 import 'dart:convert';
-// import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'package:http/http.dart' as http;
 import 'package:api_testing_dashboard/json_strings.dart';
 import 'package:api_testing_dashboard/dart_convert/converted_complex_object.dart';
 // import 'package:api_testing_dashboard/dart_convert/converted_simple_object.dart';
 import 'package:api_testing_dashboard/utils.dart';
 import 'package:api_testing_dashboard/widgets.dart';
 
-void main() => runApp(MyApp());
+Future<Status> fetchData() async {
+  final response = await http.get('http://localhost:4567/statuses/proteus');
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'API Status',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LandingPage());
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+    return Status.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
   }
 }
 
-class LandingPage extends StatelessWidget {
-  LandingPage({Key key}) : super(key: key);
+class Status {
+  final String name;
+  final int totalTests;
+  final int totalErrors;
+  final int totalFailures;
+  final int totalSkips;
+  final String rawData;
+
+  Status(
+      {
+      this.name,
+      this.totalTests,
+      this.totalErrors,
+      this.totalFailures,
+      this.totalSkips,
+      this.rawData});
+
+  factory Status.fromJson(Map<String, dynamic> json) {
+    return Status(
+        name: json['name']
+        totalTests: json['totalTests'],
+        totalErrors: json['totalErrors'],
+        totalFailures: json['totalFailures'],
+        totalSkips: json['totalSkips'],
+        rawData: json['rawData']);
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //       title: 'API Status',
+  //       theme: ThemeData(
+  //         primarySwatch: Colors.blue,
+  //       ),
+  //       home: LandingPage());
+  // }
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Status> apitStatus;
 
   @override
+  void initState() {
+    super.initState();
+    apitStatus = fetchData();
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: 'API Status',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
       appBar: AppBar(
         // Here we take the value from the LandingPage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text('API Testing Dashboard'),
       ),
       body: Center(
-        child: ConvertedListPage(),
+        child: FutureBuilder<Status>(
+          future: apitStatus,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.name);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            return CircularProgressIndicator();
+          }
+        ),
       ),
+    ),
     );
   }
 }
--- sinatra/server.rb
+++ sinatra/server.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 require "sinatra/json"
 require 'ox'
-
+require 'pry'
 get '/statuses/proteus' do
   xml = File.read(File.expand_path('proteus_report.xml', File.dirname(__FILE__)))
   res = Ox.load(xml, mode: :hash)
-  json res
+  data = {
+    name: "proteus",
+    totalTests: res[:testsuite][0][:tests],
+    totalErrors: res[:testsuite][0][:errors],
+    totalFailures: res[:testsuite][0][:failures],
+    totalSkips: res[:testsuite][0][:skips],
+    rawData: res[:testsuite],
+  }
+  json data
 end
