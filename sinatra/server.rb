235402031bfef5db3565653d8ac74cabd89ea476
Andrey Lomaka <andrey.lomaka@blacklane.com>
2019-12-13 16:48:43 +0100

    reports and endpoints

--- sinatra/server.rb
+++ sinatra/server.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
-require "sinatra/json"
+require 'sinatra/json'
 require 'ox'
 require 'pry'
+
 get '/statuses/proteus' do
   xml = File.read(File.expand_path('proteus_report.xml', File.dirname(__FILE__)))
   res = Ox.load(xml, mode: :hash)
@@ -14,4 +15,27 @@ get '/statuses/proteus' do
     rawData: res[:testsuite],
   }
   json data
+end
+
+get '/statuses' do
+  data = []
+  Dir.glob("../reports/*").each do |path|
+    xml = File.read(path)
+    res = Ox.load(xml, mode: :hash)
+    data << {
+      name: File.basename(path),
+      totalTests: res[:testsuite][0][:tests],
+      totalErrors: res[:testsuite][0][:errors],
+      totalFailures: res[:testsuite][0][:failures],
+      totalSkips: res[:testsuite][0][:skips],
+      rawData: res[:testsuite],
+    }
+  end
+  json(data)
+end
+
+get '/reports' do
+  files = Dir.glob("../reports/*").map { |f| File.basename(f) }
+
+  json(reports: files)
 end
